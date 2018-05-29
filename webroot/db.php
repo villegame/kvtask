254d23cd15ea661444c589883719354ff5ae60ea
ville <ville.niskavaara@gmail.com>
2018-05-29 17:57:50 +0300

    Initial commit of db-class.

---
+++ webroot/db.php
@@ -0,0 +1,62 @@
+<?php
+
+class DB {
+
+    private $dbConfigData;
+    private $conn;
+
+    function __construct() {
+
+        // Read database credentials and details from file outside webroot
+        $dbConfigJson = file_get_contents('../db.json');
+        $this->dbConfigData = json_decode($dbConfigJson, true);
+
+        // Connect
+        $this->connect();
+    }
+
+    function __destruct() {
+
+        $this->disconnect();
+
+    }
+
+    function connect() {
+
+        // Connect to database
+        $this->conn = new mysqli($this->dbConfigData["server"], $this->dbConfigData["user"], $this->dbConfigData["password"], $this->dbConfigData["database"]);
+        if($this->conn->connect_error) die ("Connection failed! " . $this->conn->connect_error);
+
+    }
+
+    function disconnect() {
+
+        // Close connection
+        $this->conn->close();
+
+    }
+
+    function addEmail($email) {
+
+        $sql = "INSERT INTO addresses (email) VALUES (?);";
+
+        // Prepare statement (to avoid sql injections just in case)
+        $stmt = $this->conn->prepare($sql);
+        $stmt->bind_param("s", $email);
+        $stmt->execute();
+    }
+
+    function getEmails() {
+
+        $sql = "SELECT email FROM addresses;";
+        $result = $this->conn->query($sql);
+        if ($result->num_rows > 0) {
+            while($row = $result->fetch_assoc()) {
+                print "email: " . $row["email"] . "<br />";
+            }
+        }
+    }
+
+}
+
+?>
